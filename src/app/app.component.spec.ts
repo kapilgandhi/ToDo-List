c86f4825c132babff4709819e8fb791c3799066c
kapil gandhi <kapil.gandhi@algosystech.com>
2021-04-29 07:13:09 +0530

    Initial commit for TODO list

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -20,16 +21,61 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-app'`, () => {
+  it(`should have as title 'Todo list'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('Todo list');
   });
 
-  it('should render title', () => {
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('Todo list app is running!');
+  // });
+
+  it(`should add object to the list`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-app app is running!');
+    const app = fixture.componentInstance;
+    app.newTodo = 'My First task is to clean';
+    app.addToList();
+    expect(app.todos.length).toEqual(1);
   });
+
+  it(`should remove object from the list`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.removeItem(1);
+    expect(app.todos.length).toEqual(0);
+  });
+
+  it(`should mark task as completed from the list`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const data = {
+      newTodo: 'My First task is to clean',
+      completed: false
+    };
+    app.itemCompleted(data, 0);
+    expect(app.todos[0].completed).toBeTruthy();
+  });
+
+  it(`should edit the value`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const data = 'My first Task is partially completed';
+    app.editItem(data, 0);
+    expect(app.newTodo).toEqual('My first Task is partially completed');
+    expect(app.editedItemIndex).toEqual(0);
+  });
+
+  it(`should save the edited value`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.newTodo = 'My first Task is completed';
+    app.editedItemIndex = 0;
+    app.saveToList();
+    expect(app.todos[0].newTodo).toEqual('My first Task is completed');
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Todo list'; // Title name of component
+
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+  editValue = false;
+  editedItemIndex: any;
+
+  constructor() {
+    this.newTodo = '';
+    this.todos = [];
+  }
+  
+  // add item to the list and clear the search box
+  public addToList(): void {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    };
+    this.todos.push(this.todoObj);
+    this.newTodo = '';
+  }
+
+  // remove item from the list
+  // @index - pass index of item that needs to be removed
+  public removeItem(index: number): void {
+    this.todos.splice(index, 1);
+  }
+
+  // check if item is checked, if checked then mark item as completed
+  // @item - pass item object which is checked
+  // @index - pass index of item that needs to be marked as completed
+  public itemCompleted(item: any, index: any): void {
+    this.todoObj = item;
+    this.todoObj.completed = !item.completed;
+    this.todos[index] = this.todoObj;
+  }
+
+  // edit the selected item
+  // @value : new updated value 
+  // @index: index where value us updated
+  public editItem(value: string, index: any): void {
+    this.newTodo = value;
+    this.editValue = true;
+    this.editedItemIndex = index;
+  }
+  
+  // save the new updated value
+  public saveToList(): void {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    };
+    this.todos[this.editedItemIndex] = this.todoObj;
+    this.editValue = false;
+    this.editedItemIndex = undefined;
+  }
 }
