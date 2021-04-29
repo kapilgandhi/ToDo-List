f07b3d83ab1785cd78863287e161833d8921baae
kapil gandhi <kapil.gandhi@algosystech.com>
2021-04-29 06:51:14 +0530

    updated README & docs for each method

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todo list';
+  title = 'Todo list'; // Title name of component
 
   newTodo: string;
   todos: any;
@@ -18,7 +18,8 @@ export class AppComponent {
     this.newTodo = '';
     this.todos = [];
   }
-
+  
+  // add item to the list and clear the search box
   public addToList(): void {
     this.todoObj = {
       newTodo: this.newTodo,
@@ -28,22 +29,31 @@ export class AppComponent {
     this.newTodo = '';
   }
 
+  // remove item from the list
+  // @index - pass index of item that needs to be removed
   public removeItem(index: number): void {
     this.todos.splice(index, 1);
   }
 
+  // check if item is checked, if checked then mark item as completed
+  // @item - pass item object which is checked
+  // @index - pass index of item that needs to be marked as completed
   public itemCompleted(item: any, index: any): void {
     this.todoObj = item;
     this.todoObj.completed = !item.completed;
     this.todos[index] = this.todoObj;
   }
 
+  // edit the selected item
+  // @value : new updated value 
+  // @index: index where value us updated
   public editItem(value: string, index: any): void {
     this.newTodo = value;
     this.editValue = true;
     this.editedItemIndex = index;
   }
-
+  
+  // save the new updated value
   public saveToList(): void {
     this.todoObj = {
       newTodo: this.newTodo,
